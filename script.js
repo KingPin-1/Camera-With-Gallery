1df8453694db0f6085cf89fd5c565c9574088c81
KING-PIN <mohdathar1991@gmail.com>
2022-02-25 13:39:17 +0530

    Video with Audio works

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 let video = document.querySelector("video");
+let recordBtnContainer = document.querySelector(".record-btn-container");
+let captureBtnContainer = document.querySelector(".capture-btn-container");
+let recordBtn = document.querySelector(".record-btn");
+let captureBtn = document.querySelector(".capture-btn");  
+let recordFlag = false;
+
+let chunks = [];
 
 let constraints = {
     video:true ,
@@ -9,4 +16,36 @@ let constraints = {
 navigator.mediaDevices.getUserMedia(constraints)
 .then((stream) =>{
     video.srcObject = stream;
+
+    recorder = new MediaRecorder(stream);
+
+    recorder.addEventListener("start" , (e) =>{
+        chunks = [];
+    })
+    
+    recorder.addEventListener("dataavailable", (e) => {
+        chunks.push(e.data);
+    })
+    
+    recorder.addEventListener("stop" , (e) =>{
+        //conversion of media chunks data to video
+        let blob = new Blob(chunks , {type:"video/mp4"});
+        let videoURL = URL.createObjectURL(blob);
+        let a = document.createElement("a");
+        a.href = videoURL;
+        a.download = "stream.mp4";
+        a.click();
+    })
+})
+
+recordBtnContainer.addEventListener("click" , (e) =>{
+    if( !recorder ) return ;
+    recordFlag = !recordFlag;
+    if(recordFlag){ // start
+        recorder.start();
+        recordBtn.classList.add("scale-record");
+    }else{ // stop
+        recorder.stop();
+        recordBtn.classList.remove("scale-record");
+    }
 })
